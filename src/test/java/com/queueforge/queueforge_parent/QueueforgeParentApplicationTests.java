a4e22efdd09d6bd8d02e5d9203eceb5bbdf2b816
imranfarooq-dev <imranfarooq.dev@gmail.com>
2026-05-08 19:58:16 +0500

    Initial Spring Boot setup for QueueForge

---
+++ src/test/java/com/queueforge/queueforge_parent/QueueforgeParentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.queueforge.queueforge_parent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QueueforgeParentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
